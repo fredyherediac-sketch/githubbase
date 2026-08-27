63174785c970a9818a053d180ca1efd617f7daab
fredyherediac-sketch <fredy.herediac@uniagustiniana.edu.co>
2026-08-27 15:14:53 +0000

    script

---
+++ script.js
@@ -0,0 +1,17 @@
+var texto = "tre"
+var numero = 345
+var booleano = true 
+var lista = ["dsa", "dsad"]
+var objeto = {"nombre":"alejandro" }
+
+function mifuncion(){
+    var texto;
+    if (parametro > texto){
+        texto = true
+    } else {
+        texto = false
+    }
+    return texto
+}
+
+console.log(mifuncion(7))
